1335e27b07b23455df186305d57b9aae8a1ccde6
Nathaniel McCallum <nathaniel@profian.com>
2022-07-01 10:18:10 -0400

    docs: add copyright header
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
+// SPDX-License-Identifier: AGPL-3.0-only
+
 use std::collections::HashMap;
 use std::fs::read;
 use std::io::Write;

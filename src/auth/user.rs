e166c8cd7c6108c116f3491b404fdeae32145b8c
Nathaniel McCallum <nathaniel@profian.com>
2022-07-26 05:00:16 -0400

    feat: cache star lookups
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/auth/user.rs
+++ src/auth/user.rs
@@ -1,12 +1,17 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use std::collections::HashMap;
+use std::time::Duration;
+
 use axum::async_trait;
 use axum::extract::{FromRequest, RequestParts};
 use axum::response::Response;
 use once_cell::sync::Lazy;
 use reqwest::{Client, ClientBuilder};
 use serde::Deserialize;
+use tokio::sync::RwLock;
+use tokio::time::sleep;
 use tracing::error;
 
 use crate::reference::Ref;
@@ -17,6 +22,10 @@ static HTTP: Lazy<Client> = Lazy::new(|| {
     const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
     ClientBuilder::new().user_agent(USER_AGENT).build().unwrap()
 });
+
+const STAR_TIMEOUT: Duration = Duration::from_secs(6 * 60 * 60);
+static STAR: Lazy<RwLock<HashMap<(usize, &'static str), bool>>> =
+    Lazy::new(|| HashMap::new().into());
 
 #[derive(Debug, Deserialize)]
 struct Repo {
@@ -41,7 +50,11 @@ impl<T: 'static + Send + Sync, B: Send> FromRequest<B> for Ref<User<T>> {
 }
 
 impl<T> User<T> {
-    pub async fn is_starred(&self, repo: &str) -> bool {
+    pub async fn is_starred(&self, repo: &'static str) -> bool {
+        if let Some(star) = STAR.read().await.get(&(self.uid, repo)) {
+            return *star;
+        }
+
         let result = HTTP
             .get(&format!("https://api.github.com/user/{}/starred", self.uid))
             .send();
@@ -53,7 +66,18 @@ impl<T> User<T> {
             }
 
             Ok(response) => match response.json::<Vec<Repo>>().await {
-                Ok(repos) => repos.iter().any(|r| r.full_name == repo),
+                Ok(repos) => {
+                    let star = repos.iter().any(|r| r.full_name == repo);
+
+                    let uid = self.uid;
+                    STAR.write().await.insert((uid, repo), star);
+                    tokio::spawn(async move {
+                        sleep(STAR_TIMEOUT).await;
+                        STAR.write().await.remove(&(uid, repo));
+                    });
+
+                    star
+                }
 
                 Err(e) => {
                     error!("error parsing github star status: {}", e);

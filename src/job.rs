e54300224ec97acde4dec2eef8911262deb94513
Roman Volosatovs <roman@profian.com>
2022-08-24 18:27:24 +0200

    fix: rework job management
    - Fix a few race conditions and resource leaks
    - Remove redundant code
    - Reduce lock contention
    - Rely on OS for used port tracking
    - Default to unprivileged port range
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- src/job.rs
+++ src/job.rs
@@ -1,200 +1,180 @@
 // SPDX-FileCopyrightText: 2022 Profian Inc. <opensource@profian.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-use crate::ports;
+use super::Workload;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
+use std::future::Future;
+use std::ops::Range;
 use std::process::Stdio;
-use std::str::FromStr;
 
+use anyhow::{anyhow, Context};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
-
-use anyhow::anyhow;
-use tempfile::NamedTempFile;
-use tokio::io::AsyncReadExt;
+use futures_util::future::{AbortHandle, Abortable};
+use rand::RngCore;
 use tokio::process::{Child, Command};
 use tracing::{debug, error};
-use uuid::Uuid;
 
 #[derive(Debug)]
-pub(crate) enum WorkloadType {
-    Drawbridge,
-    Upload,
-}
-
-impl FromStr for WorkloadType {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s {
-            "drawbridge" => WorkloadType::Drawbridge,
-            "upload" => WorkloadType::Upload,
-            _ => return Err(anyhow!("Unknown workload type {s}")),
-        })
-    }
-}
-
-pub(crate) enum Standard {
-    Output,
-    Error,
-}
-
-#[derive(Debug)]
-#[allow(dead_code)]
 pub(crate) struct Job {
-    pub(crate) uuid: Uuid,
-    exec: Child,
-    workload_type: WorkloadType,
-    slug: Option<String>,
-    wasm: Option<NamedTempFile>,
-    toml: Option<NamedTempFile>,
-
     /// OCI container engine command
     oci_command: OsString,
-    mapped_ports: HashMap<u16, u16>,
+    destructor: AbortHandle,
+    workload: Workload,
+
+    pub(crate) id: String,
+    pub(crate) exec: Child,
+    pub(crate) mapped_ports: HashMap<u16, u16>,
 }
 
-impl Drop for Job {
-    fn drop(&mut self) {
-        if !self.mapped_ports.is_empty() {
-            error!("a job was not cleaned up correctly");
-        }
-    }
+#[cfg(target_os = "linux")]
+async fn used_ports<T: FromIterator<u16>>(ss: impl AsRef<OsStr>) -> anyhow::Result<T> {
+    use std::io::{BufRead, BufReader};
+    use std::net::{Ipv4Addr, SocketAddr};
+
+    let out = Command::new(ss)
+        .arg("-ltnH")
+        .output()
+        .await
+        .context("failed to run `ss`")?;
+    BufReader::new(out.stdout.as_slice())
+        .lines()
+        .map(|s| {
+            s.context("failed to read line")?
+                .split_whitespace()
+                .nth(3)
+                .ok_or_else(|| anyhow!("address column missing"))?
+                .replace('*', &Ipv4Addr::UNSPECIFIED.to_string())
+                .parse()
+                .context("failed to parse socket address")
+                .map(|addr| match addr {
+                    SocketAddr::V4(addr) => addr.port(),
+                    SocketAddr::V6(addr) => addr.port(),
+                })
+        })
+        .collect()
 }
 
 impl Job {
-    pub(crate) fn new(
-        oci_image_tag: OsString,
-        workload_type: String,
-        slug: Option<String>,
-        wasm: Option<NamedTempFile>,
-        toml: Option<NamedTempFile>,
+    /// Spawns a new job via selected OCI engine, it is not safe for concurrent use.
+    #[allow(clippy::too_many_arguments)]
+    pub(crate) async fn spawn(
+        id: String,
+        workload: Workload,
+        ss_command: impl AsRef<OsStr>,
         oci_command: OsString,
-        mapped_ports: HashMap<u16, u16>,
+        oci_image: &str,
+        port_range: Range<u16>,
+        ports: impl IntoIterator<Item = u16>,
+        destructor: impl Future<Output = ()> + Send + 'static,
     ) -> Result<Self, Response> {
-        let uuid = Uuid::new_v4();
-        let workload_type = WorkloadType::from_str(&workload_type).map_err(|e| {
-            debug!("Failed to parse workload type: {e}");
-            StatusCode::BAD_REQUEST.into_response()
-        })?;
-        let mapped_ports_args = mapped_ports
-            .iter()
-            .flat_map(|(container_port, host_port)| {
-                ["-p".to_string(), format!("{host_port}:{container_port}")]
-            })
-            .collect::<Vec<_>>();
-        let extra_args = env::var("ENARX_BACKEND")
-            .map(|var| ["-e".to_string(), format!("ENARX_BACKEND={}", var)])
-            .unwrap_or_default();
-        let exec = match workload_type {
-            WorkloadType::Drawbridge => {
-                let slug = slug
-                    .as_ref()
-                    .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
-                Command::new(&oci_command)
-                    .args(&["run", "--rm", "--name"])
-                    .arg(&uuid.to_string())
-                    .args(mapped_ports_args)
-                    .args(extra_args)
-                    .arg(oci_image_tag)
-                    .arg("enarx")
-                    .arg("deploy")
-                    .arg(slug)
-                    .stdin(Stdio::null())
-                    .stdout(Stdio::piped())
-                    .stderr(Stdio::piped())
-                    .kill_on_drop(true)
-                    .spawn()
-                    .map_err(|e| {
-                        error!("failed to spawn process: {e}");
-                        StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                    })?
+        debug!("spawning a job. id={id} workload={:?}", workload);
+        let mut cmd = Command::new(&oci_command);
+        let mut cmd = cmd
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .kill_on_drop(true)
+            .args(["run", "--rm", "--name", id.as_str()]);
+
+        if let Some(backend) = env::var_os("ENARX_BACKEND") {
+            let mut var = OsString::from("ENARX_BACKEND=");
+            var.push(backend);
+            cmd = cmd.arg("-e").arg(var);
+        }
+
+        let ports: Vec<_> = ports.into_iter().collect();
+        let port_count = ports.len();
+        let mapped_ports = if port_count > 0 {
+            let used: HashSet<_> = used_ports(ss_command).await.map_err(|e| {
+                error!("failed to lookup used ports: {e}");
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            })?;
+            let start = port_range.start
+                + (rand::thread_rng().next_u32() as usize % port_range.len()) as u16;
+            let mapped: HashMap<_, _> = (start..port_range.end)
+                .chain(port_range.start..start)
+                .into_iter()
+                .filter(|p| !used.contains(p))
+                .zip(ports)
+                .collect();
+            if mapped.len() < port_count {
+                return Err((
+                    StatusCode::SERVICE_UNAVAILABLE,
+                    "Insufficient amount of open ports on the system, try again later",
+                )
+                    .into_response());
             }
-            WorkloadType::Upload => {
-                let wasm = wasm
-                    .as_ref()
-                    .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
-                let wasm_path_str = wasm.path().to_str().ok_or_else(|| {
-                    error!("Failed to get wasm path as str");
-                    StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                })?;
-                let toml = toml
-                    .as_ref()
-                    .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
-                let toml_path_str = toml.path().to_str().ok_or_else(|| {
-                    error!("Failed to get toml path as str");
-                    StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                })?;
-                Command::new(&oci_command)
-                    .args(&["run", "--rm", "--name"])
-                    .arg(&uuid.to_string())
-                    .arg("-v")
-                    .arg(format!("{}:/app/Enarx.toml", toml_path_str))
-                    .arg("-v")
-                    .arg(format!("{}:/app/main.wasm", wasm_path_str))
-                    .args(mapped_ports_args)
-                    .args(extra_args)
-                    .arg(oci_image_tag)
-                    .arg("enarx")
-                    .arg("run")
-                    .arg("--wasmcfgfile")
-                    .arg("/app/Enarx.toml")
-                    .arg("/app/main.wasm")
-                    .stdin(Stdio::null())
-                    .stdout(Stdio::piped())
-                    .stderr(Stdio::piped())
-                    .kill_on_drop(true)
-                    .spawn()
-                    .map_err(|e| {
-                        error!("failed to spawn process: {e}");
-                        StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                    })?
-            }
+            cmd = mapped.iter().fold(cmd, |cmd, (host, cont)| {
+                cmd.arg("-p").arg(format!("{host}:{cont}"))
+            });
+            mapped
+        } else {
+            Default::default()
         };
 
+        let cmd = match &workload {
+            Workload::Drawbridge { slug } => {
+                cmd.args([oci_image, "enarx", "deploy", slug.as_str()])
+            }
+            Workload::Upload { wasm, conf } => cmd.args([
+                "-v",
+                format!("{}:/app/Enarx.toml", conf.path().to_string_lossy()).as_str(),
+                "-v",
+                format!("{}:/app/main.wasm", wasm.path().to_string_lossy()).as_str(),
+                oci_image,
+                "enarx",
+                "run",
+                "--wasmcfgfile",
+                "/app/Enarx.toml",
+                "/app/main.wasm",
+            ]),
+        };
+        debug!("spawning a job run command. cmd={:?}", cmd);
+        let exec = cmd.spawn().map_err(|e| {
+            error!("failed to start job: {e}");
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        })?;
+
+        let (destructor_tx, destructor_rx) = AbortHandle::new_pair();
+        _ = tokio::spawn(Abortable::new(destructor, destructor_rx));
         Ok(Self {
-            uuid,
+            id,
             exec,
-            workload_type,
-            slug,
-            wasm,
-            toml,
             oci_command,
             mapped_ports,
+            workload,
+            destructor: destructor_tx,
         })
     }
 
-    pub(crate) async fn read(
-        &mut self,
-        kind: Standard,
-        buffer: &mut [u8],
-    ) -> std::io::Result<usize> {
-        match kind {
-            Standard::Output => self.exec.stdout.as_mut().unwrap().read(buffer).await,
-            Standard::Error => self.exec.stderr.as_mut().unwrap().read(buffer).await,
-        }
-    }
-
-    pub(crate) async fn kill(&mut self) {
-        match Command::new(&self.oci_command)
+    pub(crate) async fn kill(mut self) {
+        self.destructor.abort();
+        if let Ok(mut child) = Command::new(&self.oci_command)
             .arg("kill")
-            .arg(&self.uuid.to_string())
+            .arg(&self.id)
             .spawn()
+            .map_err(|e| {
+                error!("failed to spawn kill process: {e} job_id={}", self.id);
+            })
         {
-            Err(e) => {
-                error!("failed to run docker kill {}", e);
-            }
-            Ok(mut child) => {
-                if let Err(e) = child.wait().await {
-                    error!("failed to run docker kill {}", e);
-                }
+            if let Err(e) = child.wait().await {
+                error!("failed to kill job: {e} job_id={}", self.id);
             }
         }
         let _ = self.exec.kill().await;
-        ports::free(&self.mapped_ports).await;
-        self.mapped_ports.clear();
+        if let Workload::Upload { wasm, conf } = self.workload {
+            debug!("closing `main.wasm`");
+            if let Err(e) = wasm.close() {
+                error!("failed to close `main.wasm`: {e}. job_id={}", self.id);
+            };
+            debug!("closing `Enarx.toml`");
+            if let Err(e) = conf.close() {
+                error!("failed to close `Enarx.toml`: {e}. job_id={}", self.id);
+            };
+        }
     }
 }
